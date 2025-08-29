82cdb2eb168f506e29de74eb31359f8c966fd8bf
Emaan Hookey <em.grey28@gmail.com>
2025-08-29 13:35:51 -0230

    initial commit

---
+++ serve.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const mimeTypes = {
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.scss': 'text/css', // Using SCSS directly as CSS for development
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.gif': 'image/gif',
+};
+
+const server = http.createServer((req, res) => {
+  console.log(`Request for ${req.url}`);
+
+  let filePath =
+    req.url === '/'
+      ? path.join(__dirname, 'src', 'index.html')
+      : path.join(__dirname, 'src', req.url);
+
+  const extname = path.extname(filePath);
+  let contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      console.error(`Error: ${err.message}`);
+
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('404 Not Found');
+      } else {
+        res.writeHead(500, { 'Content-Type': 'text/html' });
+        res.end('500 Server Error');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
